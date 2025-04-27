d5b6d0d2ad39041c257c5813a49bfcac904a323b
Noomen Krichen <76108353+noomenkrichen@users.noreply.github.com>
2025-04-27 08:07:05 +0100

    Update tmux-conf.sh

--- tmux-conf.sh
+++ tmux-conf.sh
@@ -1,5 +1,7 @@
 echo "========================================================="
-echo "=          Installation of tmux configuration           ="
+echo "=          Installation of tmux Configuration           ="
+echo "=          -< Automation by Noomen Krichen >-           ="
+echo "=                      April 2025                       ="
 echo "========================================================="
 echo "=                              >>> Downloading files... ="
 git clone https://github.com/noomenkrichen/my_dotfiles.git "${XDG_CONFIG_HOME:-$HOME}/temp/tmux"
