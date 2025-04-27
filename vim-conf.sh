6a025938369325a1c4a16cc5739c5f19e9d930f0
Noomen Krichen <76108353+noomenkrichen@users.noreply.github.com>
2025-04-27 09:01:11 +0100

    Update vim-conf.sh

--- vim-conf.sh
+++ vim-conf.sh
@@ -3,15 +3,14 @@ echo "=           Installation of Vim Configuration           ="
 echo "=          -< Automation by Noomen Krichen >-           ="
 echo "=                      April 2025                       ="
 echo "========================================================="
-echo -e "\e[31mThis text is red\e[0m"
-echo "=                              >>> Downloading files... ="
+echo "=\e[36m                              >>> Downloading files... \e[0m="
 git clone https://github.com/noomenkrichen/my_dotfiles.git "${XDG_CONFIG_HOME:-$HOME}/temp/vim"
-echo "=             >>> Installing Vim configuration files... ="
+echo "=\e[36m             >>> Installing Vim configuration files... ="
 mv ${XDG_CONFIG_HOME:-$HOME}/temp/vim/vim/.vimrc ${XDG_CONFIG_HOME:-$HOME}/
 mv ${XDG_CONFIG_HOME:-$HOME}/temp/vim/vim/.vim ${XDG_CONFIG_HOME:-$HOME}/.vim
-echo "=               >>> Cleaning tmux installation files... ="
+echo "=\e[36m               >>> Cleaning tmux installation files... \e[0m="
 sudo rm -r ${XDG_CONFIG_HOME:-$HOME}/temp/vim
-echo "=               >>> Installation complete with success! ="
-echo "= Now open Vim and enjoy your new Vim configuration!    ="
+echo "=\e[36m               >>> Installation complete with success! \e[0m="
+echo "=\e[32m Now open Vim and enjoy your new Vim configuration!    \e[0m="
 echo "========================================================="
 vim
