d28c55dd41b4deb97ee02221334c669618fb91b7
Noomen Krichen <76108353+noomenkrichen@users.noreply.github.com>
2025-04-27 09:05:08 +0100

    Update vim-conf.sh

--- vim-conf.sh
+++ vim-conf.sh
@@ -5,7 +5,7 @@ echo "=                      April 2025                       ="
 echo "========================================================="
 echo "=\e[36m                              >>> Downloading files... \e[0m="
 git clone https://github.com/noomenkrichen/my_dotfiles.git "${XDG_CONFIG_HOME:-$HOME}/temp/vim"
-echo "=\e[36m             >>> Installing Vim configuration files... ="
+echo "=\e[36m             >>> Installing Vim configuration files... \e[0m="
 mv ${XDG_CONFIG_HOME:-$HOME}/temp/vim/vim/.vimrc ${XDG_CONFIG_HOME:-$HOME}/
 mv ${XDG_CONFIG_HOME:-$HOME}/temp/vim/vim/.vim ${XDG_CONFIG_HOME:-$HOME}/.vim
 echo "=\e[36m               >>> Cleaning tmux installation files... \e[0m="
