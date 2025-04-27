38f14e5c6ae4cb7d7e3bec9225b6bad8bb893d40
Noomen Krichen <76108353+noomenkrichen@users.noreply.github.com>
2025-04-27 09:14:17 +0100

    Update vim-conf.sh

--- vim-conf.sh
+++ vim-conf.sh
@@ -8,7 +8,7 @@ git clone https://github.com/noomenkrichen/my_dotfiles.git "${XDG_CONFIG_HOME:-$
 echo "\e[35m=\e[36m             >>> Installing VIM configuration files... \e[35m=\e[0m"
 mv ${XDG_CONFIG_HOME:-$HOME}/temp/vim/vim/.vimrc ${XDG_CONFIG_HOME:-$HOME}/
 mv ${XDG_CONFIG_HOME:-$HOME}/temp/vim/vim/.vim ${XDG_CONFIG_HOME:-$HOME}/.vim
-echo "\e[35m=\e[36m               >>> Cleaning VIM installation files... \e[35m=\e[0m"
+echo "\e[35m=\e[36m                >>> Cleaning VIM installation files... \e[35m=\e[0m"
 sudo rm -r ${XDG_CONFIG_HOME:-$HOME}/temp/vim
 echo "\e[35m=\e[36m                    >>> Installation complete success! \e[35m="
 echo "=\e[32m Now open VIM and enjoy your new Vim configuration!    \e[35m="
