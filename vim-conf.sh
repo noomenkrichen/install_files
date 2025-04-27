eb181fd7490f1bd9153a401f7989b2dd8e83c0c2
Noomen Krichen <76108353+noomenkrichen@users.noreply.github.com>
2025-04-27 09:18:37 +0100

    Update vim-conf.sh

--- vim-conf.sh
+++ vim-conf.sh
@@ -5,6 +5,8 @@ echo "=                      April 2025                       ="
 echo "========================================================="
 echo "=\e[36m                              >>> Downloading files... \e[35m=\e[0m"
 git clone https://github.com/noomenkrichen/my_dotfiles.git "${XDG_CONFIG_HOME:-$HOME}/temp/vim"
+echo "\e[35m=\e[36m                >>> Remove old configuration of VIM... \e[35m=\e[0m"
+sudo rm -r ${XDG_CONFIG_HOME:-$HOME}/.vim
 echo "\e[35m=\e[36m             >>> Installing VIM configuration files... \e[35m=\e[0m"
 mv ${XDG_CONFIG_HOME:-$HOME}/temp/vim/vim/.vimrc ${XDG_CONFIG_HOME:-$HOME}/
 mv ${XDG_CONFIG_HOME:-$HOME}/temp/vim/vim/.vim ${XDG_CONFIG_HOME:-$HOME}/.vim
