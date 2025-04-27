73c3c96f6e1408f82310cb2d76bec8700fc1b048
Noomen Krichen <76108353+noomenkrichen@users.noreply.github.com>
2025-04-27 08:04:10 +0100

    Update tmux-conf.sh

--- tmux-conf.sh
+++ tmux-conf.sh
@@ -5,7 +5,7 @@ echo "=                              >>> Downloading files... ="
 git clone https://github.com/noomenkrichen/my_dotfiles.git "${XDG_CONFIG_HOME:-$HOME}/temp/tmux"
 echo "=                     >>> Installing .tmux.conf file... ="
 mv ${XDG_CONFIG_HOME:-$HOME}/temp/tmux/tmux/.tmux.conf ${XDG_CONFIG_HOME:-$HOME}/
-echo "=            >>> Cleaning temporary downloaded files... ="
+echo "=               >>> Cleaning tmux installation files... ="
 sudo rm -r ${XDG_CONFIG_HOME:-$HOME}/temp/tmux
 echo "=               >>> Installation complete with success! ="
 echo "========================================================="
